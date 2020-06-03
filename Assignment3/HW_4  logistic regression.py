6c43073f7f84bb5b7c015a46cbbb3d9ce47aadfb
yuvalgeva77 <yuvalgeva77@gmail.com>
2020-06-04 02:38:23 +0300

    J,H,f lambdas

--- Assignment3/HW_4  logistic regression.py
+++ Assignment3/HW_4  logistic regression.py
@@ -8,38 +8,38 @@ from mlxtend.data import mnist_data
 import loadMNIST
 from numpy.linalg import inv
 
-#f,J is the function matrix
+#f,J is  lambdas
 def armijo(x,f,d,J, b=0.5, a_0=1, c=1,max_iter=100):
     a_k = a_0
     for i in range(0,max_iter):
-        f_k = mul(f,x + a_k * d)
-        limit = mul(f,x) + c * a_k * mul(mul(J,x).transpose(),d)
+        f_k = mul(f(x) + a_k * d)
+        limit = f(x) + c * a_k * mul(J(x).transpose(),d)
         if f_k <= limit:
            break
         a_k = a_k * b
     return a_k
 
-#H, matricie
+#H, lambdas
 def gradient_descent(x_0, H, eps = 0.00001,a = 0.01):
     cur_x = x_0  # The algorithm starts at x0
     previous_step_size = cur_x
     while previous_step_size > eps:
         prev_x = cur_x
-        cur_x += -a * mul(H,prev_x)
+        cur_x += -a * H(prev_x)
         previous_step_size = abs(cur_x - prev_x)#TODO check if abs works on vector
     return cur_x
 
-#J,H,f matricies
+#J,H,f lambdas
 def exact_Newton(f, H,J, x_0,  eps = 0.00001):
     x_k=x_0
     while abs(f(x_k)) > eps:
         a=armijo(x_k, f, J)
-        d_n=mul(inv(H(x_k)),mul(J,x_k))
+        d_n=mul(inv(H(x_k)),J(x_k))
         x_k = x_k +a*d_n
     return x_k
 
 def logistic_func(x_i, w):
-    return 1 / 1 + math.exp(mul(-x_i, w))
+    return lambda w: 1 / 1 + math.exp(mul(-x_i, w))
 
 
 def logistic_func_Marix(X, w):
@@ -48,7 +48,7 @@ def logistic_func_Marix(X, w):
     for i in range(0, m):
         x_i = X[:, i]  # ith First Column
         res[i] = 1 / 1 + math.exp(mul(-x_i, w))
-    return res
+    return lambda w:res(w)
 
 
 def logistic_regression_objective(X, y, w):
@@ -56,13 +56,13 @@ def logistic_regression_objective(X, y, w):
     res = 0
     for i in range(0, m):
         x_i = X[:, i]  # ith First Column
-        res = +(y[i] * math.log2(logistic_func(x_i.transpose(), w)) + 1 - y[i]) * math.log2(1 - logistic_func(x_i, w))
-    return (res / m)
+        res =+(y[i] * math.log2(logistic_func(x_i.transpose(), w)) + 1 - y[i]) * math.log2(1 - logistic_func(x_i, w))
+    return lambda w:res(w) / m
 
 
 def gradient(X, Y, w):
     n, m = X.shape  # columns
-    return ((X.mul(logistic_func_Marix(X.transpose(), w) - y[0]) / m))
+    return lambda w:X.mul(logistic_func_Marix(X.transpose(), w) - Y[0]) / m
 
 
 def hessian(X, w):
@@ -72,12 +72,11 @@ def hessian(X, w):
     for i in range(0, m):
         res = d1[i] * d2[i]
     D = p.diag(res)
-    return ((X.mul(D.mul(X.transpose()))) / m)
+    return lambda w:X.mul(D.mul(X.transpose())) / m
 
 
 def task_4a(X, labels):
-    w = np.array([1 / 50, 1 / 50, 1 / 50])
-    J = logistic_regression_objective(X, labels, w)
+    J = logistic_regression_objective(X, labels)
     print("logistic_regression_objective  {0}\n".format(J))
     G = gradient(X, labels, w)
     print("gradient is:  {0}\n".format(G))
@@ -87,5 +86,6 @@ def task_4a(X, labels):
 
 
 (X, labels) = loadMNIST.random_shuffeled_Mnist()
-
-# task_4a()
+w=np.ones(X.shape[])
+(J, G, H)=task_4a(X, labels)
+G(w)
