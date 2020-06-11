c3d2756f897378ebfdf1ec752ac9b894a62cac73
yuvalgeva77 <yuvalgeva77@gmail.com>
2020-06-12 00:35:36 +0300

    4c

--- Assignment3/loadMNIST.py
+++ Assignment3/loadMNIST.py
@@ -8,13 +8,13 @@ def loadMnist():  # return [train, test]
     # Dimensions x: 5000 x 784
     # Dimensions: y: 5000
     X, Y = mnist_data()
-    nSamples = 30000;  # Number of samples from the dataset
+    nSamples = 30000 # Number of samples from the dataset
     X_t = X.transpose()
     X = X[0:nSamples]
     Y = Y[0:nSamples]
     # Digits to classify
-    d1 = 0;
-    d2 = 1;
+    d1 = 0
+    d2 = 1
     # Take only d1 and d2 digits
     filtered_data = np.array([X[i] for i in range(0, X.shape[0]) if (Y[i] == d1 or Y[i] == d2)])  # (1000, 1)
     filtered_labels = np.array([[Y[i]] for i in range(0, X.shape[0]) if (Y[i] == d1 or Y[i] == d2)])  # (1000, 784)
