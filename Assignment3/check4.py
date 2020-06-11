e8a84ce4c21a88edf9b569a6060e1c073c005fca
yuvalgeva77 <yuvalgeva77@gmail.com>
2020-06-11 12:12:12 +0300

    Update check4.py

--- Assignment3/check4.py
+++ Assignment3/check4.py
@@ -65,7 +65,7 @@ def make_rand_vector(dims):
     mag = sum(x ** 2 for x in vec) ** .5
     return [x / mag for x in vec]
 
-
+#-------------------
 (X, labels) = loadMNIST.random_shuffeled_Mnist()
 n, m = X.shape
 d = np.array(make_rand_vector(n))
@@ -95,15 +95,17 @@ for i in range(1, limit):
     w_i = w_0 + eps_i * d
     res1 = abs(f(X, labels, w_i) - f(X, labels, w_0))
     res2 = abs((f(X, labels, w_i) - f(X, labels, w_0) - eps_i * mul(d.transpose(), (gradient(X, labels, w_0)))))
+
     if (abs((res_1_prev / res1) - 1 / factor) > 1 or abs((res_2_prev / res2) - 1 / (factor ** 2)) > 1):
         print("failed The gradient test with eps:{0}, iteration {1}".format(eps, i))
+
     res3 = np.linalg.norm(gradient(X, labels, w_i) - gradient(X, labels,
                                                              w_0))
     res4 = np.linalg.norm(((gradient(X, labels, w_i) - gradient(X, labels, w_0)).transpose() - eps_i * mul(
-        d.transpose(), (hessian(X, w_0)))))  
-#todo abs((res_4_prev / res4) - 1 / (factor ** 2)) is about 3-4 not 1
+        d.transpose(), (hessian(X, w_0)))))
+#todo abs((res_4_prev / res4) - 1 / (factor ** 2)) is about 3-4 not 1 check whay 2*(factor ** 2)
     if (abs((res_3_prev / res3) - 1 / factor) > 1 or abs((res_4_prev / res4) - 1 / (factor ** 2)) > 1):
-        print("failed The gradient test with eps:{0}, iteration {1}, res is ({2},{3})\n".format(eps, i,abs((res_3_prev / res3) - 1 / factor),abs((res_4_prev / res4) - 1 / (factor ** 2))))
+        print("failed The Jacobian test with eps:{0}, iteration {1}, res is ({2},{3})\n".format(eps, i,abs((res_3_prev / res3) - 1 / factor),abs((res_4_prev / res4) - 1 / (factor ** 2))))
     res_1_prev = res1
     res_2_prev = res2
     res_3_prev = res3
